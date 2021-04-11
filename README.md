33ea1499948fcfd30124accddd6c2eb1d5d57355
dave <sunrong1990@126.com>
2021-04-11 22:54:12 +0800

    python的leetcode之旅启动

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PythonLife
 Dave Sun的Python生活，我的主旋律
+
+## 代码工具和格式
+统一使用的pycharm，Python版本3.8
+
+## 参考资料
+leetcode：https://leetcode-cn.com/
+开课吧，船长课程，https://learn.kaikeba.com/
---
+++ leetcode-life/array/NO_1_two_sum_easy.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mapping = {}
+
+        for index, val in enumerate(nums):
+            diff = target - val
+            if diff in mapping:
+                return [index, mapping[diff]]
+            else:
+                mapping[val] = index
