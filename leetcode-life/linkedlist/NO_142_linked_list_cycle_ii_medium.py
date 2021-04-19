80a7b0a9ac61f12744fe3b56145663d29595eda7
dave <sunrong1990@126.com>
2021-04-20 07:16:08 +0800

    python的链表问题1

---
+++ leetcode-life/linkedlist/NO_142_linked_list_cycle_ii_medium.py
@@ -0,0 +1,25 @@
+from myLinkedList import *
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        """
+        一道典型的数学证明题
+        将距离划分为a b c 断，a+b 获得相遇的点，证明a = c
+        判断链表中环的位置
+        :param head:
+        :return:
+        """
+        if not head:
+            return None
+        start,slow,fast= head,head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast= fast.next.next
+            if slow and slow == fast:
+                while start != slow:
+                    start = start.next
+                    slow =slow.next
+                return start
+        return None
+
+
