f90af7c9b0d1bf2afc4fe29bb16fd95702f006bd
Dave <sunrong1990@126.com>
2026-05-30 13:56:58 +0800

    越来越熟练的回溯练习

--- leetcode-life/back-track/NO_17_phone_num_medium.py
+++ leetcode-life/back-track/NO_17_phone_num_medium.py
@@ -28,6 +28,29 @@ class Solution:
                 path.pop()
         trackback(0)
         return ret
+    def letterCombinations2(self, digits: str) -> List[str]:
+            # 数字到字母的映射
+        mapping = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+        # path is the oombination of digits's letter
+        # 可选列表：每个字母所代替的字母表
+        n = len(digits)
+        ret  = []
+        path =[]
+        def backtrack(start):
+            if start == n:
+                path.append(path)
+                return
+            # 可选列表，树的第二层
+            for c in mapping[digits[start]]:
+                path.append(c)
+                backtrack(start+1)
+                path.pop()
+        backtrack(0)
+        return ret
+        
 
 
 s = Solution()
--- leetcode-life/back-track/NO_39_combination-sum_medium.py
+++ leetcode-life/back-track/NO_39_combination-sum_medium.py
@@ -4,13 +4,50 @@ from typing import List
 组合综合
 
 方法1
-
+@date : 2022
+@update: 2026-5-30
 """
-
-
-def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
-    ret = []
-    if not candidates:
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # 路径：数字和为target的-数字组合
+        ret = []
+        # 可选列表：每层都是一样的
+        path = []
+        n = len(candidates)
+        
+        def backtrack(start):
+            if sum(path) == target:
+                ret.append(path[:])
+                return
+            if len(path) > 150:
+                return
+            if sum(path) > target:
+                return
+            # 可选列表
+            # 注意：为了避免重复，只能向后取数据
+            for i in range(start, n):
+                path.append(candidates[i])
+                backtrack(i)
+                path.pop()
+        
+        backtrack(0)
         return ret
-    candidates.sort()
+    
+    # 上面方法的优化：优化反复和运算
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # 路径：数字和为target的-数字组合
+        ret = []
+        # 可选列表：每层都是一样的
+        path = []
+        n = len(candidates)
+        
+        def backtrack(start):
+           return
+        
+        backtrack(0)
+        return ret
+            
+s = Solution()
+candidates = [2, 3, 6, 7]
+print(s.combinationSum(candidates, 7))
     
