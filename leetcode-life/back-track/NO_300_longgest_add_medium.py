72fc9edb2882da84e0e7a22284e99d3375626e8e
Dave <sunrong1990@126.com>
2026-06-02 13:47:50 +0800

    回溯法练习

---
+++ leetcode-life/back-track/NO_300_longgest_add_medium.py
@@ -0,0 +1,15 @@
+from typing import List
+
+"""
+回溯法
+最长递增子序列
+"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        path = []
+
+
+s = Solution()
+print(s.lengthOfLIS("aab"))
